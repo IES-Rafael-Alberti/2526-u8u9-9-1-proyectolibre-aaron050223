3231694006996da1615d2abfa2c93c571d71ac66
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-05-29 15:32:08 +0200

    Edito Main.kt para que usar el DAO de H2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,20 @@
 package org.iesra
 
-import org.iesra.dao.memory.ReservaDAOMemory
+import org.iesra.config.DatabaseManager
+import org.iesra.dao.bdd.ReservaDAOH2
 import org.iesra.service.PabellonService
 import org.iesra.ui.MenuTerminal
 
 /** Punto de entrada de la aplicacion. */
 fun main() {
+    val databaseManager = DatabaseManager()
+    databaseManager.inicializarBBDD()
 
-    val dao = ReservaDAOMemory()
+    val dao = ReservaDAOH2()
     val servicio = PabellonService(dao)
     val ui = MenuTerminal(servicio)
+
+    servicio.limpiarReservasAntiguas()
 
     var bucle = true
     do {
