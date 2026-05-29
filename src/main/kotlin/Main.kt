1c4bb83376abdbf352049a8f4f8e829761e10e2a
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-05-29 15:31:25 +0200

    Añado Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 package org.iesra
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.iesra.dao.memory.ReservaDAOMemory
+import org.iesra.service.PabellonService
+import org.iesra.ui.MenuTerminal
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+/** Punto de entrada de la aplicacion. */
+fun main() {
+
+    val dao = ReservaDAOMemory()
+    val servicio = PabellonService(dao)
+    val ui = MenuTerminal(servicio)
+
+    var bucle = true
+    do {
+        val continuar = ui.iniciarFlujoReserva()
+        if (!continuar) bucle = false
+    } while (bucle)
 }
