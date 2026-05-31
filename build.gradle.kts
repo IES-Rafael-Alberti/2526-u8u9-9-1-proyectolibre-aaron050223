27ab0a94d955952605deaf729b1cae24834707ab
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-05-31 15:51:11 +0200

    Edito las dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib"))
     implementation("com.h2database:h2:2.2.224")
+    implementation("org.mongodb:mongodb-driver-sync:5.2.0")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
 }
 
 kotlin {
