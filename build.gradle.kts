ff1aa3cc6854966cdafa2bae86ac643d12e88859
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-06-01 18:09:37 +0200

    Edito las dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     implementation(kotlin("stdlib"))
     implementation("com.h2database:h2:2.2.224")
     implementation("org.mongodb:mongodb-driver-sync:5.2.0")
